8b69f7b0c7068f602edac1818c23ea8c449bf790
Gchevallier45 <34574840+Gchevallier45@users.noreply.github.com>
2018-04-18 01:26:46 +0200

    Delete Boss.java

--- src/guillaume/projet/java/company/Boss.java
+++
@@ -1,9 +0,0 @@
-package guillaume.projet.java.company;
-
-public class Boss extends Person {
-
-	public Boss(String firstName, String lastName, String phone, int age, String email) {
-		super(firstName, lastName, phone, age, email);
-		// TODO Auto-generated constructor stub
-	}
-}
